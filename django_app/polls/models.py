700dab67addde8e3cefa185d6e35e70d13e166b5
Andrew Subotin <subotinandrey5@gmail.com>
2022-12-17 11:16:48 +0200

    models of polls

--- django_app/polls/models.py
+++ django_app/polls/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Question(models.Model):
+
+    question_text = models.CharField(max_length=128)
+    published_at = models.DateTimeField('published at')
+
+
+class Choice(models.Model):
+
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=128)
+    votes = models.IntegerField(default=0)
