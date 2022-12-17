bd91662ab34599d25bdcca8c661ac905e8895221
Andrew Subotin <subotinandrey5@gmail.com>
2022-12-17 11:03:42 +0200

    first view of second app

--- django_app/django_app/urls.py
+++ django_app/django_app/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls'))
+    path('polls/', include('polls.urls')),
+    path('shopping_cart/', include('shopping_cart.urls'))
 ]
